a97be1eb6fdbfbc7085109be9b7d7ff2542dc7a8
Michael Wong <michael.wong@dxc.com>
2024-08-12 15:18:38 +0800

    Update 10_Reference (Class).py

--- pages/10_Reference (Class).py
+++ pages/10_Reference (Class).py
@@ -92,11 +92,8 @@ ssic_sl = ssic_4_sl
 st.set_page_config(
     page_title='ssicsync', # Set display name of browser tab
     page_icon="🔍", # Set display icon of browser tab
-    layout="wide", # "wide" or "centered"
-    initial_sidebar_state="expanded",
-    menu_items={
-        'About': '''Explore multiclass text classification with DistilBERT on our Streamlit page. 
-        Discover interactive insights and the power of modern NLP in text categorization!'''
+    layout="centered", # "wide" or "centered"
+    initial_sidebar_state="expanded"
     }
 )
 
