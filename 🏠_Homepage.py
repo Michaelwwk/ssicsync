78666e0edd86bb8189aa233a17789c073071392c
Michael Wong <michael.wong@dxc.com>
2024-08-12 15:44:19 +0800

    Update 🏠_Homepage.py

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -36,7 +36,7 @@ st.markdown(custom_styles, unsafe_allow_html=True)
 col1, col2, col3 = st.columns([1, 10, 1])  # Adjust column proportions as needed
 
 with col1:
-    st.image('image/ACRA_logo2.jpg', caption='', output_format='JPEG', width=100)  # Set width to shrink the image
+    st.image('image/ACRA_logo2.jpg', caption='', output_format='JPEG', width=200)  # Set width to shrink the image
 
 st.write("## Welcome to ssicsync streamlit page! 👋")
 
