0779a42c9a47785faa0daac600eb9448ba896f56
Michael Wong <michael.wong@dxc.com>
2024-08-14 10:58:15 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from controller import controllerService
 
 # hard-coded variables
-level = 'Subclass'
-topN = 3
-max_files = 100
+level = 'Subclass' # this refers to the model hierarchical type used to obtain the list of companies' SSIC results.
+topN = 3 # this refers to the top N number of SSIC codes that each companies SSIC codes should be within, to be considered as a hit (therefore classifying correctly). this score affects the prediction accuracy.
+max_files = 100 # this refers to the max log files recoded in the repository.
 
 modelResults = controllerService(level, topN, max_files)
 logger = modelResults.setup_logger('main')
--- models/classificationModel/validatingClassificationModel.py
+++ models/classificationModel/validatingClassificationModel.py
@@ -12,7 +12,7 @@ def validatingClassificationModel(self, logger):
     level = self.level
     topN = self.topN
     list_df_filepath = r"dataSources/ScrapedOutputFiles/list of 90 Coy and SSIC.csv"
-    vdf_filepath = r"LLM_Test/Summarised_output_for_model.xlsx" # TODO change path name and file name eventually!
+    vdf_filepath = r"LLM_Test/Summarised_output_for_model_v3.xlsx" # TODO change path name and file name eventually!
     ssic_detailed_def_filepath = r"dataSources/DoS/ssic2020-detailed-definitions.xlsx"
     ssic_alpha_index_filepath = r"dataSources/DoS/ssic2020-alphabetical-index.xlsx"
 
