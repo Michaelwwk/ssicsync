c0faee2f035f08df23eb902882135fe15daab2c2
Michael Wong <michael.wong@dxc.com>
2024-08-20 10:55:52 +0800

    Update 1_Results.py

--- pages/1_Results.py
+++ pages/1_Results.py
@@ -302,7 +302,7 @@ col7, col8 = st.columns([1,1])
 with col7:
     st.write("")
 with col8:
-    st.write(f"<p style='font-size:12px; text-align:right;'><sup>*</sup>{adjustedWeightDef}</p>", unsafe_allow_html=True)
+    st.write(f"<p style='font-size:12px; text-align:left;'>*{adjustedWeightDef}</p>", unsafe_allow_html=True)
 
 # Visual Effects ### - https://docs.streamlit.io/develop/api-reference/status
 # st.balloons() 
