9e8c18f78b59994c72f6efd6ffb8c4beeac8e645
Michael Wong <michael.wong@dxc.com>
2024-08-20 10:59:56 +0800

    Update 1_Results.py

--- pages/1_Results.py
+++ pages/1_Results.py
@@ -300,8 +300,7 @@ else:
 
 col7, col8 = st.columns([1,1])
 with col7:
-    st.write("\n\n\n")
-    st.write(f"<p style='font-size:12px; text-align:left;'>*{adjustedWeightDef}</p>", unsafe_allow_html=True)
+    st.write(f"<p style='font-size:12px; text-align:left; margin-top:20px;'>*{adjustedWeightDef}</p>", unsafe_allow_html=True)
 with col8:
     st.write("")
 
