c3edbbb242e8551b1a395d8470240e45d7c9d7b0
Michael Wong <michael.wong@dxc.com>
2024-08-21 23:09:43 +0800

    Update 1_Results.py

--- pages/1_Results.py
+++ pages/1_Results.py
@@ -161,7 +161,7 @@ with col2:
 # Streamlit selectbox for user input
 level_input = st.selectbox(
     "Level of Classification:",
-    categories
+    [section, division, group, Class, subclass]
 )
 level = level_input if level_input else section
 
