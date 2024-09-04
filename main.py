09a7d623265a762dde95310b6d717276be9457bf
Michael Wong <michael.wong@dxc.com>
2024-09-04 10:54:42 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # hard-coded variables
 modelChoice = 'fb_bart_tfidf' # ('sd_azma_bart', 'azma_bart_tfidf', 'sd_philschmid_bart', 'philschmid_bart_tfidf', 'sd_fb_bart', 'fb_bart_tfidf', 'QA') ...
 # ... this refers to the chosen summary model of choice.
-level = 'Section' # ('Section', 'Division', 'Group', 'Class', 'Subclass') ... #TODO level section and resultslevel section got prob!
+level = 'Group' # ('Section', 'Division', 'Group', 'Class', 'Subclass') ...
 # ... this refers to chosen SSIC hierarchical model to obtain the list of companies' SSIC results.
-resultsLevel = 'Section' # this refers to the hierarchical level to be seen in final results file ...
+resultsLevel = 'Group' # this refers to the hierarchical level to be seen in final results file ...
 # ... 'resultsLevel' has to be equal or less granular than 'level'! ('Section', 'Division', 'Group', 'Class', 'Subclass')
 topN = 3 # this refers to the top N number of SSIC codes that each companies SSIC codes ...
 # ... should be within to be considered as a hit (therefore classifying correctly). this score affects the prediction accuracy.
@@ -31,6 +31,6 @@ if __name__ == "__main__":
     logger.info('Start code execution ...')
     logger.info(f'Summary model: {modelChoice}, Classification model: {level}, Results level: {resultsLevel}, Top N: {topN}')
 
-    modelResults.runPdfExtraction(logger)
-    modelResults.runSummaryModel(logger)
+    modelResults.runPdfExtraction(logger) # comment out this line to skip step (hard-coded values does not affect here)
+    modelResults.runSummaryModel(logger) # comment out this line to skip step (hard-coded values does not affect here)
     modelResults.runValidatingClassificationModel(logger, ssic_detailed_def_filepath, ssic_alpha_index_filepath, companies_filepath)
